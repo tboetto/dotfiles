26f178858c71e60d35a8ae444cb64c51e9aa2d45
Tony Boetto <tony.boetto@cvshealth.com>
2026-04-09 20:08:32 -0400

    feat: add node certs for copilot

--- emacs/.config/emacs/tony-emacs-modules/tony-emacs-miscellaneous.el
+++ emacs/.config/emacs/tony-emacs-modules/tony-emacs-miscellaneous.el
@@ -1,7 +1,8 @@
 (use-package
- exec-path-from-shell
- :ensure t
- :config
- (when (memq window-system '(mac ns x))
-   (exec-path-from-shell-initialize)))
+  exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
+    (add-to-list 'exec-path-from-shell-variables "NODE_EXTRA_CA_CERTS")
+    (exec-path-from-shell-initialize)))
 (provide 'tony-emacs-miscellaneous)
